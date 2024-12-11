bb0775425429e083399523f1797e0e419a4ff633
jovanb21324 <purpledroptop1@gmail.com>
2024-12-11 13:12:00 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,13 @@ input.onGesture(Gesture.Shake, function () {
     hand = randint(1, 3)
     if (hand == 1) {
         basic.showIcon(IconNames.SmallSquare)
+        finch.setBeak(100, 0, 0)
     } else if (hand == 2) {
         basic.showIcon(IconNames.Square)
+        finch.setBeak(0, 100, 0)
     } else {
         basic.showIcon(IconNames.Scissors)
+        finch.setBeak(0, 0, 100)
     }
 })
 let hand = 0

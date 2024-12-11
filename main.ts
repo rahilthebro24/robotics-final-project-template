30e51f8482d1083cf3a78973b8e84ca38f38ac77
jovanb21324 <purpledroptop1@gmail.com>
2024-12-11 13:28:06 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
     hand = randint(1, 3)
     if (hand == 1) {
         basic.showIcon(IconNames.SmallSquare)
-        finch.setBeak(100, 0, 0)
+        finch.setBeak(0, 0, 100)
     } else if (hand == 2) {
         basic.showIcon(IconNames.Square)
         finch.setBeak(0, 100, 0)
+        finch.setMove(MoveDir.Forward, 10, 50)
+        finch.setTurn(RLDir.Right, 90, 50)
+        finch.setMove(MoveDir.Forward, 10, 50)
+        finch.setTurn(RLDir.Right, 90, 50)
+        finch.setMove(MoveDir.Forward, 10, 50)
+        finch.setTurn(RLDir.Right, 90, 50)
+        finch.setMove(MoveDir.Forward, 10, 50)
+        finch.setTurn(RLDir.Right, 90, 50)
     } else {
         basic.showIcon(IconNames.Scissors)
-        finch.setBeak(0, 0, 100)
+        finch.setBeak(100, 0, 0)
     }
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })
 let hand = 0
 finch.startFinch()

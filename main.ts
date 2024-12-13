3e0764ebc1863e823cfb9605689ea59d72a08ea1
jovanb21324 <purpledroptop1@gmail.com>
2024-12-13 12:17:30 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,8 @@ input.onButtonPressed(Button.A, function () {
             . # # # .
             # . . . #
             # . . . #
+            # . . . #
             . # # # .
-            . . . . .
             `)
         for (let index = 0; index < 1; index++) {
             finch.startMotors(0, 50)
@@ -16,31 +16,12 @@ input.onButtonPressed(Button.A, function () {
     } else if (hand == 2) {
         Paper()
     } else {
-        basic.showIcon(IconNames.Scissors)
-        finch.setBeak(0, 100, 0)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Left, 33, 50)
-        finch.setMove(MoveDir.Forward, 20, 100)
-        finch.setMove(MoveDir.Backward, 20, 50)
-        finch.setTurn(RLDir.Right, 33, 50)
-        finch.setBeak(0, 100, 0)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Right, 90, 50)
-        finch.setMove(MoveDir.Forward, 10, 50)
-        finch.setTurn(RLDir.Left, 33, 50)
-        finch.setMove(MoveDir.Forward, 20, 50)
-        finch.setMove(MoveDir.Backward, 20, 50)
+        scissors()
     }
+    finch.stopMotors()
+})
+input.onButtonPressed(Button.AB, function () {
+    finch.stopMotors()
 })
 function Paper () {
     basic.showIcon(IconNames.Square)
@@ -53,6 +34,46 @@ function Paper () {
     finch.setTurn(RLDir.Right, 90, 50)
     finch.setMove(MoveDir.Forward, 10, 50)
     finch.setTurn(RLDir.Right, 90, 50)
+}
+input.onButtonPressed(Button.B, function () {
+    finch.setMove(MoveDir.Forward, 10, 50)
+    finch.setMove(MoveDir.Backward, 10, 50)
+    finch.setTail(TailPort.One, 100, 0, 0)
+    basic.pause(1000)
+    finch.setBeak(0, 0, 100)
+    basic.pause(1000)
+    finch.setTail(TailPort.One, 0, 100, 0)
+    basic.pause(1000)
+    finch.setBeak(100, 0, 0)
+    basic.pause(1000)
+    finch.setMove(MoveDir.Forward, 10, 50)
+    basic.pause(100)
+})
+function scissors () {
+    basic.showIcon(IconNames.Scissors)
+    finch.setBeak(0, 100, 0)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Left, 16, 50)
+    finch.setMove(MoveDir.Forward, 10, 100)
+    finch.setMove(MoveDir.Backward, 10, 50)
+    finch.setTurn(RLDir.Right, 16, 50)
+    finch.setBeak(0, 100, 0)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Right, 45, 50)
+    finch.setMove(MoveDir.Forward, 5, 50)
+    finch.setTurn(RLDir.Left, 16, 50)
+    finch.setMove(MoveDir.Forward, 10, 50)
+    finch.setMove(MoveDir.Backward, 10, 50)
 }
 let hand = 0
 finch.startFinch()
